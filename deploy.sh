89cc336fb5b879cf3f09a00576184d06441b41d4
fengxuan.liu <fengxuan.liu@nttdata.com>
2018-12-10 11:48:15 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ ps -ef|grep birth-topic-1.0-SNAPSHOT.jar|awk '{print $2}'|xargs kill -9
 
 mvn package
 
-cp target/birth-topic-1.0-SNAPSHOT.jar ./birth-topic1.0-SNAPSHOT.jar
+cp target/birth-topic-1.0-SNAPSHOT.jar ./birth-topic-1.0-SNAPSHOT.jar
 
 nohup java -jar ./birth-topic-1.0-SNAPSHOT.jar > ./nohup.out &
 
